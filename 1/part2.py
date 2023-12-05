2fc55feabfe340a54f6d8ff13ad35dafd5e74e85
Théo <14152836+theovdhd@users.noreply.github.com>
2023-12-05 22:17:20 +0100

    part 2 - not sure why it's incorrect yet

---
+++ 1/part2.py
@@ -0,0 +1,20 @@
+numbers = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+
+f = open('input.txt', 'r')
+
+sum = 0
+for line in f.readlines():
+    print('-------------------')
+    print(line)
+    indexes = {}
+    for number in numbers:
+        if number in line:
+            indexes[line.find(number)] = numbers.index(number) + 1
+    for i in range(len(line)):
+        if line[i].isdigit():
+            indexes[i] = int(line[i])
+    indexes = dict(sorted(indexes.items()))
+    calibration_value = int(list(indexes.values())[0]) * 10 + int(list(indexes.values())[-1])
+    print(calibration_value)
+    sum += calibration_value
+print(sum)
